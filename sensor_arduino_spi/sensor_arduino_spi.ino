3de8bb8f535b1ef442450960b535c6731ff23dd8
j0shua000 <matias.armijo2000@gmail.com>
2023-05-09 21:26:27 -0400

    eliminacion de codigos inutilizados en el repositorio

--- sensor_arduino_spi/sensor_arduino_spi.ino
+++ sensor_arduino_spi/sensor_arduino_spi.ino
@@ -5,41 +5,47 @@
 const int TEMP_SENSOR_PIN = 2;
 OneWire oneWire(TEMP_SENSOR_PIN);
 DallasTemperature sensors(&oneWire);
-
-
 DeviceAddress deviceAddress;
 
 const int SLAVE_SELECT_PIN = 10;
-volatile bool newDataAvailable = false;
-volatile float temperature = 0;
+float temperature;
 
 void setup() {
+  Serial.begin(9600);  
   sensors.begin();
   sensors.getAddress(deviceAddress, 0);
   sensors.setResolution(deviceAddress, 12);
 
-  pinMode(SLAVE_SELECT_PIN, INPUT);
-  pinMode(MISO, OUTPUT);
-  SPI.attachInterrupt();
+  pinMode(SLAVE_SELECT_PIN, OUTPUT);
+  digitalWrite(SLAVE_SELECT_PIN, HIGH);
   SPI.begin();
-  SPCR |= _BV(SPE) | _BV(SPIE);
+  SPI.setClockDivider(SPI_CLOCK_DIV8); // Ajusta la velocidad del reloj si es necesario
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setBitOrder(MSBFIRST);
 }
 
 void loop() {
   sensors.requestTemperatures();
   temperature = sensors.getTempC(deviceAddress);
-  newDataAvailable = true;
+  sendData(temperature);
+  Serial.println(temperature);
   delay(30000);
 }
 
-ISR(SPI_STC_vect) {
-  if (newDataAvailable) {
-    int16_t tempToSend = temperature * 100;
-    byte highByte = highByte(tempToSend);
-    byte lowByte = lowByte(tempToSend);
-    SPDR = highByte;
+void sendData(float temperature) {
+  int16_t tempToSend = temperature * 100;
+  byte highByte = highByte(tempToSend);
+  byte lowByte = lowByte(tempToSend);
+
+  byte dataToSend[] = {'S', highByte, lowByte, 'E'};
+
+  digitalWrite(SLAVE_SELECT_PIN, LOW);
+  delayMicroseconds(50);
+
+  for (byte data : dataToSend) {
+    SPI.transfer(data);
     delayMicroseconds(50);
-    SPDR = lowByte;
-    newDataAvailable = false;
   }
+
+  digitalWrite(SLAVE_SELECT_PIN, HIGH);
 }
